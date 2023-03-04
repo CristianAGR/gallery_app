794330a54f9a44b85c1ac91f498f4ffab04d99cc
CristianAGR <cgr72677@udelasalle.edu.mx>
2023-03-04 00:01:46 -0600

    shows the selected image

--- lib/screens/photo_screen.dart
+++ lib/screens/photo_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:gallery_app/providers/images_services.dart';
+import 'package:photo_manager/photo_manager.dart';
 
 class PhotoScreen extends StatelessWidget {
    
@@ -6,17 +8,30 @@ class PhotoScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    final String image = ModalRoute.of(context)!.settings.arguments as String;
+    // recibes the entity argument
+    final AssetEntity entity = ModalRoute.of(context)!.settings.arguments as AssetEntity;
+    var services = ImagesServices(); 
+
+    // Icon change when is selected
+    IconData favIcon = Icons.favorite_border;
+     bool iconChange(AssetEntity entity) {
+      favIcon = services.addFavorite(favIcon);
+       print("New favorite");
+       //favIcon = Icons.favorite;
+     return entity.isFavorite;
+     }
+    
+
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
         title: Row(
           children: [
             IconButton(onPressed: () => Navigator.pop(context), icon: const Icon(Icons.arrow_back_ios_new, color: Colors.black,)),
-            const Text("17/02/23", style: TextStyle(color: Colors.black),)
+            Text(services.formatDate(entity), style: const TextStyle(color: Colors.black),)
             ],
         ),
-        backgroundColor: Color.fromARGB(0, 53, 51, 51),
+        backgroundColor: const Color.fromARGB(0, 53, 51, 51),
         shadowColor: Colors.transparent,
       ),
       body: Center(
@@ -24,13 +39,14 @@ class PhotoScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             //https://cdn.pixabay.com/photo/2023/01/05/22/35/flower-7700011_640.jpg
-            Image(image: NetworkImage(image)),
+            // recibe la imagen seleccionada y la muestra
+            AssetEntityImage(entity),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                IconButton(onPressed: addFavorite, icon: Icon(Icons.delete)),
-                IconButton(onPressed: addFavorite, icon: Icon(Icons.favorite_border)),
-                IconButton(onPressed: addFavorite, icon: Icon(Icons.menu))
+                IconButton(onPressed: () => services.deleteEntity(entity), icon: const Icon(Icons.delete)),
+                IconButton(onPressed: () => iconChange(entity), icon: Icon(favIcon)),
+                IconButton(onPressed: () => iconChange(entity), icon: const Icon(Icons.menu))
               ],
             ),
             Row(
@@ -47,6 +63,11 @@ class PhotoScreen extends StatelessWidget {
     );
   }
 
-  void addFavorite() {
-  }
+  
+  
+
+
+
+
+
 }
