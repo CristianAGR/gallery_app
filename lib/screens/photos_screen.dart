6160895e6acc94bf4b6367f24b09716bdff760c4
CristianAGR <cgr72677@udelasalle.edu.mx>
2023-03-04 00:02:13 -0600

    shows all paths in device with a button

--- lib/screens/photos_screen.dart
+++ lib/screens/photos_screen.dart
@@ -1,9 +1,139 @@
 import 'package:flutter/material.dart';
+import 'package:gallery_app/providers/images_services.dart';
 import 'package:gallery_app/widgets/image_widget.dart';
+import 'package:photo_manager/photo_manager.dart';
 
-class PhotosScreen extends StatelessWidget {
+class PhotosScreen extends StatefulWidget {
    
   const PhotosScreen({Key? key}) : super(key: key);
+
+  @override
+  State<PhotosScreen> createState() => _PhotosScreenState();
+}
+
+class _PhotosScreenState extends State<PhotosScreen> {
+
+  var services = ImagesServices();
+
+  
+  /// Customize your own filter options.
+  final FilterOptionGroup _filterOptionGroup = FilterOptionGroup(
+    imageOption: const FilterOption(
+      sizeConstraint: SizeConstraint(ignoreSize: true),
+    ),
+  );
+  final int _sizePerPage = 50;
+
+  AssetPathEntity? _path;
+  List<AssetEntity>? _entities;
+  int _totalEntitiesCount = 0;
+
+  int _page = 0;
+  bool _isLoading = false;
+  bool _isLoadingMore = false;
+  bool _hasMoreToLoad = true;
+
+
+  // gets assets
+  Future<void> _requestAssets() async {
+    setState(() {
+      _isLoading = true;
+    });
+    // Request permissions.
+    final PermissionState ps = await PhotoManager.requestPermissionExtend();
+    if (!mounted) {
+      return;
+    }
+    // Further requests can be only proceed with authorized or limited.
+    if (!ps.hasAccess) {
+      setState(() {
+        _isLoading = false;
+      });
+      //showToast('Permission is not accessible.');
+      print('Permission is not accessible.');
+      return;
+    }
+    print("Cargando: $_isLoading" );
+    // Obtain assets using the path entity.
+    final List<AssetPathEntity> paths = await PhotoManager.getAssetPathList(
+      onlyAll: true,
+      filterOption: _filterOptionGroup,
+    );
+    if (!mounted) {
+      return;
+    }
+    // Return if not paths found.
+    if (paths.isEmpty) {
+      setState(() {
+        _isLoading = false;
+      });
+      //showToast('No paths found.');
+      print('No paths found.');
+      return;
+    }
+    setState(() {
+      _path = paths.first;
+    });
+    _totalEntitiesCount = await _path!.assetCountAsync;
+    print("Paths encontrados: $_totalEntitiesCount");
+    final List<AssetEntity> entities = await _path!.getAssetListPaged(
+      page: 0,
+      size: _sizePerPage,
+    );
+    if (!mounted) {
+      return;
+    }
+    setState(() {
+      _entities = entities;
+      _isLoading = false;
+      _hasMoreToLoad = _entities!.length < _totalEntitiesCount;
+    });
+  }
+
+  Future<void> _loadMoreAsset() async {
+    final List<AssetEntity> entities = await _path!.getAssetListPaged(
+      page: _page + 1,
+      size: _sizePerPage,
+    );
+    if (!mounted) {
+      return;
+    }
+    setState(() {
+      _entities!.addAll(entities);
+      _page++;
+      _hasMoreToLoad = _entities!.length < _totalEntitiesCount;
+      _isLoadingMore = false;
+    });
+  }
+
+  // builds the response of device paths
+  Widget _buildBody(BuildContext context) {
+
+    //services.assetVerification(_isLoading, _path, _entities);
+    if (_isLoading) {
+      return const Center(child: CircularProgressIndicator.adaptive());
+    }
+    if (_path == null) {
+      return const Center(child: Text('Request paths first.'));
+    }
+    if (_entities?.isNotEmpty != true) {
+      return const Center(child: Text('No assets found on this device.'));
+    }
+    
+
+    //final AssetEntity entity = _entities![index];
+    // builds the gridView of images with the found entities
+    return GridView.builder(
+                padding: const EdgeInsets.only(right: 50),
+                shrinkWrap: false,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3, childAspectRatio: 0.85),
+                itemCount: _totalEntitiesCount,
+                itemBuilder: (_, int index) => ImageWidget(
+                  entity: _entities![index],
+                  option: const ThumbnailOption(size: ThumbnailSize.square(200)),
+                  //image: "https://cdn.pixabay.com/photo/2012/08/27/14/19/mountains-55067__340.png",
+                  ));
+  }
   
   @override
   Widget build(BuildContext context) {
@@ -15,23 +145,13 @@ class PhotosScreen extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 const Text("Photos", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold), textAlign: TextAlign.center,),
-                IconButton(onPressed: options, icon: const Icon(Icons.menu), padding: EdgeInsets.only(left: 20),),
+                IconButton(onPressed: options, icon: const Icon(Icons.menu), padding: const EdgeInsets.only(left: 20),),
               ],
             ),
             Expanded(
-              child: GridView.builder(
-                padding: EdgeInsets.only(right: 50),
-                shrinkWrap: false,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3, childAspectRatio: 0.85),
-                itemCount: 20,
-                itemBuilder: (_, int index) => ImageWidget(image: "https://cdn.pixabay.com/photo/2012/08/27/14/19/mountains-55067__340.png",))
-              // ListView.builder(
-              //   scrollDirection: Axis.vertical,
-              //   itemCount: 8,
-              //   itemBuilder: (_, int index) => RowImages()
-              //   ),
+              child: _buildBody(context)
             ),
-            FloatingActionButton(onPressed: addPhoto, child: Icon(Icons.camera_alt),)
+            FloatingActionButton(onPressed: _requestAssets, child: const Icon(Icons.camera_alt),)
           ],
         ),
       )
