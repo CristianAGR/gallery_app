293a88acc2a6f75ea2e312aee0c449705806ceae
CristianAGR <cgr72677@udelasalle.edu.mx>
2023-03-04 00:04:45 -0600

    receives the entity and sets it as argument

--- lib/widgets/image_widget.dart
+++ lib/widgets/image_widget.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:photo_manager/photo_manager.dart';
 
 class ImageWidget extends StatelessWidget {
   
-  final String image;
-  const ImageWidget({super.key, required this.image});
+  
+  const ImageWidget({super.key, this.image, required this.entity,
+    required this.option,
+    this.onTap,
+    });
+
+  final String? image;
+  final AssetEntity entity;
+  final ThumbnailOption option;
+  final GestureTapCallback? onTap;
 
 
   @override
@@ -11,10 +20,13 @@ class ImageWidget extends StatelessWidget {
     return Container(
       margin: const EdgeInsets.only(left: 10, bottom: 10),
       child: GestureDetector(
-        onTap: () => Navigator.pushNamed(context, 'photo', arguments: image),
+        onTap: () => Navigator.pushNamed(context, 'photo', arguments: entity),
         child: ClipRRect(
           borderRadius: BorderRadius.circular(3),
-          child: Image(fit: BoxFit.cover, width: 80, height: 90,image: NetworkImage(image)),
+          child: AssetEntityImage(
+            entity
+            )
+          //Image(fit: BoxFit.cover, width: 80, height: 90,image: NetworkImage(image)),
         ),
       ),
     );
