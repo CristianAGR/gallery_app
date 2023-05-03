13ac9f6dd5dbc5c48e565544345d5d2cdfa63e46
CristianAGR <cgr72677@udelasalle.edu.mx>
2023-05-03 16:05:13 -0600

    shows the name of the album and number of items

--- lib/widgets/album_widget.dart
+++ lib/widgets/album_widget.dart
@@ -1,18 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:photo_manager/photo_manager.dart';
 
-class AlbumWidget extends StatelessWidget {
-  const AlbumWidget({super.key});
+class AlbumWidget extends StatefulWidget {
+  const AlbumWidget({super.key, required this.albumPath});
 
+  final AssetPathEntity albumPath;
+
+  @override
+  State<AlbumWidget> createState() => _AlbumWidgetState();
+}
+
+class _AlbumWidgetState extends State<AlbumWidget> {
+/// Customize your own filter options.
+  final FilterOptionGroup _filterOptionGroup = FilterOptionGroup(
+    imageOption: const FilterOption(
+      sizeConstraint: SizeConstraint(ignoreSize: true),
+    ),
+  );
+  final int _sizePerPage = 50;
+  List<AssetEntity>? _entities;
+  //int _totalEntitiesCount = 0;
+
+  int itemsCount = 0;
+  AssetPathEntity? _path;
+    Future<void> _getItemsCount() async {
+      //itemsCount = await widget.albumPath.assetCountAsync;
+      itemsCount = await _path!.assetCountAsync;
+      setState(() {
+        
+      });
+
+      final List<AssetEntity> entities = await _path!.getAssetListPaged(
+       page: 0,
+       size: _sizePerPage,
+     );
+     _entities = entities;
+      
+    }
+
+    // final List<AssetEntity> entities = await widget.albumPath.getAssetListPaged(
+    //   page: 0,
+    //   size: 50,
+    // );
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _path = widget.albumPath;
+    _getItemsCount();
+  }
   @override
   Widget build(BuildContext context) {
     const album = "https://cdn.pixabay.com/photo/2023/01/05/22/35/flower-7700011_640.jpg";
+    
+    
+    
     return Container(
       margin: const EdgeInsets.only(left: 10, bottom: 10),
       child: GestureDetector(
-        onTap: () => Navigator.pushNamed(context, 'album', arguments: album),
+        onTap: () => Navigator.pushNamed(context, 'album', arguments: _path),
         child: Column(
           children: [
-            const Text("Album", textAlign: TextAlign.start, style: TextStyle(fontWeight: FontWeight.bold),),
+            Text("${widget.albumPath.name}", textAlign: TextAlign.start, style: TextStyle(fontWeight: FontWeight.bold),),
             Row(
               children: const [
                 ClipRRect(
@@ -37,7 +87,7 @@ class AlbumWidget extends StatelessWidget {
                 ),
               ],
             ),
-            const Text("10 items", textAlign: TextAlign.start,),
+            Text("$itemsCount items", textAlign: TextAlign.start,),
           ],
         ),
       ),
