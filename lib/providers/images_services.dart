5a01760299f8ba24163967e9dad1ef29b1f05dbe
CristianAGR <cgr72677@udelasalle.edu.mx>
2023-05-11 19:59:19 -0600

    delete current entity function added

--- lib/providers/images_services.dart
+++ lib/providers/images_services.dart
@@ -1,9 +1,15 @@
 
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:photo_manager/photo_manager.dart';
+import 'package:provider/provider.dart';
+
+import 'images_provider.dart';
 
 class ImagesServices extends ChangeNotifier{
-
+AssetPathProvider readPathProvider(BuildContext c) =>
+      c.read<AssetPathProvider>();
 
 
   // method to return a formated date
@@ -19,9 +25,32 @@ class ImagesServices extends ChangeNotifier{
      String formatedDate =  "${dayName[date.weekday - 1]} ${date.day}/${date.month}/${date.year}";
      return formatedDate;
    }
-
-  deleteEntity(AssetEntity entity) {
-    var id = entity.id;
+      
+  Future<void> deleteCurrent(BuildContext context, AssetEntity entity) async {
+    if (Platform.isAndroid) {
+      final AlertDialog dialog = AlertDialog(
+        title: const Text('Delete the asset'),
+        actions: <Widget>[
+          TextButton(
+            child: const Text(
+              'delete',
+              style: TextStyle(color: Colors.red),
+            ),
+            onPressed: () async {
+              readPathProvider(context).delete(entity);
+              Navigator.pop(context);
+            },
+          ),
+          TextButton(
+            child: const Text('cancel'),
+            onPressed: () => Navigator.pop(context),
+          ),
+        ],
+      );
+      showDialog<void>(context: context, builder: (_) => dialog);
+    } else {
+      readPathProvider(context).delete(entity);
+    }
   }
 
    addFavorite(IconData favIcon) {
