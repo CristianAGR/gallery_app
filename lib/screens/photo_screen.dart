101a2425a65f3019716a5e6aafe58a9d2106cada
CristianAGR <cgr72677@udelasalle.edu.mx>
2023-05-11 20:13:04 -0600

    creation of AssetPathProvider, deletion of entity

--- lib/screens/photo_screen.dart
+++ lib/screens/photo_screen.dart
@@ -1,29 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:gallery_app/providers/images_services.dart';
+import 'package:gallery_app/widgets/image_widget.dart';
 import 'package:photo_manager/photo_manager.dart';
+import 'package:provider/provider.dart';
+import '../providers/images_provider.dart';
 
 class PhotoScreen extends StatelessWidget {
    
-  const PhotoScreen({Key? key}) : super(key: key);
-  
+  PhotoScreen({Key? key}) : super(key: key);
+  var services = ImagesServices(); 
+    // Icon change when is selected
+    IconData favIcon = Icons.favorite_border;
+
   @override
   Widget build(BuildContext context) {
     // recibes the entity argument
-    final AssetEntity entity = ModalRoute.of(context)!.settings.arguments as AssetEntity;
-    var services = ImagesServices(); 
+    final ScreenArguments screenArguments = ModalRoute.of(context)!.settings.arguments as ScreenArguments;
+    final AssetEntity entity = screenArguments.entity;
+    final AssetPathEntity path = screenArguments.path;
 
-    // Icon change when is selected
-    IconData favIcon = Icons.favorite_border;
-     bool iconChange(AssetEntity entity) {
-      favIcon = services.addFavorite(favIcon);
-       print("New favorite");
-       //favIcon = Icons.favorite;
-     return entity.isFavorite;
-     }
-    
-
-    return Scaffold(
-      appBar: AppBar(
+      return ChangeNotifierProvider<AssetPathProvider>(
+      create: (_) => AssetPathProvider(path),
+      builder: (BuildContext context, _) => Scaffold(
+        appBar: AppBar(
         automaticallyImplyLeading: false,
         title: Row(
           children: [
@@ -34,40 +33,39 @@ class PhotoScreen extends StatelessWidget {
         backgroundColor: const Color.fromARGB(0, 53, 51, 51),
         shadowColor: Colors.transparent,
       ),
-      body: Center(
-         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            //https://cdn.pixabay.com/photo/2023/01/05/22/35/flower-7700011_640.jpg
-            // recibe la imagen seleccionada y la muestra
-            AssetEntityImage(entity),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                IconButton(onPressed: () => services.deleteEntity(entity), icon: const Icon(Icons.delete)),
-                IconButton(onPressed: () => iconChange(entity), icon: Icon(favIcon)),
-                IconButton(onPressed: () => iconChange(entity), icon: const Icon(Icons.menu))
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: const[
-                Text("Delete"),
-                Text("Favorite"),
-                Text("Datails")
-              ],
-            )
-          ],
-         )
+        body: _buildItem(context, entity,favIcon),
       ),
     );
+
   }
 
-  
-  
-
-
-
-
+  Widget _buildItem(BuildContext context, entity,favIcon) {
+    return Center(
+       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          //https://cdn.pixabay.com/photo/2023/01/05/22/35/flower-7700011_640.jpg
+          // recibe la imagen seleccionada y la muestra
+          AssetEntityImage(entity),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(onPressed: () => services.deleteCurrent(context, entity), icon: const Icon(Icons.delete)),
+              IconButton(onPressed: () => services.iconChange(entity), icon: Icon(favIcon)),
+              IconButton(onPressed: () => services.iconChange(entity), icon: const Icon(Icons.menu))
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: const[
+              Text("Delete"),
+              Text("Favorite"),
+              Text("Datails")
+            ],
+          )
+        ],
+       )
+    );
+   }
 
 }
