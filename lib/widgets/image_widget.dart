46d8a5ca56fbdd2f64a0ff47f18d7fbfe3528cd8
CristianAGR <cgr72677@udelasalle.edu.mx>
2023-05-11 19:56:18 -0600

    sends path and entity arguments to photo_screen

--- lib/widgets/image_widget.dart
+++ lib/widgets/image_widget.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:photo_manager/photo_manager.dart';
 
+class ScreenArguments {
+  final AssetPathEntity path;
+  final AssetEntity entity;
+  ScreenArguments(this.path, this.entity);
+}
+
 class ImageWidget extends StatelessWidget {
   
   
-  const ImageWidget({super.key, this.image, required this.entity,
+  const ImageWidget({super.key, this.image, required this.path, required this.entity,
     required this.option,
     this.onTap,
     });
-
+  final AssetPathEntity path;
   final String? image;
   final AssetEntity entity;
   final ThumbnailOption option;
   final GestureTapCallback? onTap;
+
+  
 
 
   @override
@@ -20,7 +28,7 @@ class ImageWidget extends StatelessWidget {
     return Container(
       margin: const EdgeInsets.only(left: 10, bottom: 10),
       child: GestureDetector(
-        onTap: () => Navigator.pushNamed(context, 'photo', arguments: entity),
+        onTap: () => Navigator.pushNamed(context, 'photo', arguments: ScreenArguments(path, entity)),
         child: ClipRRect(
           borderRadius: BorderRadius.circular(3),
           child: AssetEntityImage(
