88f83d0ed8e88298db0c67d1a67e93b9e3ee2b01
CristianAGR <cgr72677@udelasalle.edu.mx>
2023-05-13 22:12:10 -0600

    setting of portrait view for all the application

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var services = ImagesServices();
+    services.portraitModeOnly();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Gallery',
