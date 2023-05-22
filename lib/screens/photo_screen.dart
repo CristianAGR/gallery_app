8051f14d550099df18623400ef06f326c668d198
CristianAGR <cgr72677@udelasalle.edu.mx>
2023-05-22 15:57:04 -0600

    zoomable image

--- lib/screens/photo_screen.dart
+++ lib/screens/photo_screen.dart
@@ -5,67 +5,128 @@ import 'package:photo_manager/photo_manager.dart';
 import 'package:provider/provider.dart';
 import '../providers/images_provider.dart';
 
-class PhotoScreen extends StatelessWidget {
-   
+class PhotoScreen extends StatefulWidget {
   PhotoScreen({Key? key}) : super(key: key);
-  var services = ImagesServices(); 
-    // Icon change when is selected
-    IconData favIcon = Icons.favorite_border;
+
+  @override
+  State<PhotoScreen> createState() => _PhotoScreenState();
+}
+
+class _PhotoScreenState extends State<PhotoScreen> {
+  var services = ImagesServices();
+
+  // Icon change when is selected
+  IconData favIcon = Icons.favorite_border;
 
   @override
   Widget build(BuildContext context) {
+    services.enableRotation();
     // recibes the entity argument
-    final ScreenArguments screenArguments = ModalRoute.of(context)!.settings.arguments as ScreenArguments;
+    final ScreenArguments screenArguments =
+        ModalRoute.of(context)!.settings.arguments as ScreenArguments;
     final AssetEntity entity = screenArguments.entity;
     final AssetPathEntity path = screenArguments.path;
 
-      return ChangeNotifierProvider<AssetPathProvider>(
+    return ChangeNotifierProvider<AssetPathProvider>(
       create: (_) => AssetPathProvider(path),
       builder: (BuildContext context, _) => Scaffold(
         appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Row(
-          children: [
-            IconButton(onPressed: () => Navigator.pop(context), icon: const Icon(Icons.arrow_back_ios_new, color: Colors.black,)),
-            Text(services.formatDate(entity), style: const TextStyle(color: Colors.black),)
-            ],
-        ),
-        backgroundColor: const Color.fromARGB(0, 53, 51, 51),
-        shadowColor: Colors.transparent,
-      ),
-        body: _buildItem(context, entity,favIcon),
-      ),
-    );
-
-  }
-
-  Widget _buildItem(BuildContext context, entity,favIcon) {
-    return Center(
-       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          //https://cdn.pixabay.com/photo/2023/01/05/22/35/flower-7700011_640.jpg
-          // recibe la imagen seleccionada y la muestra
-          AssetEntityImage(entity),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+          automaticallyImplyLeading: false,
+          title: Row(
             children: [
-              IconButton(onPressed: () => services.deleteCurrent(context, entity), icon: const Icon(Icons.delete)),
-              IconButton(onPressed: () => services.iconChange(entity), icon: Icon(favIcon)),
-              IconButton(onPressed: () => services.iconChange(entity), icon: const Icon(Icons.menu))
+              IconButton(
+                  onPressed: () => Navigator.pop(context),
+                  icon: const Icon(
+                    Icons.arrow_back_ios_new,
+                    color: Colors.black,
+                  )),
+              Text(
+                services.formatDate(entity),
+                style: const TextStyle(color: Colors.black),
+              )
             ],
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: const[
-              Text("Delete"),
-              Text("Favorite"),
-              Text("Datails")
-            ],
-          )
-        ],
-       )
+          backgroundColor: const Color.fromARGB(0, 53, 51, 51),
+          shadowColor: Colors.transparent,
+        ),
+        body: _buildItem(context, entity, favIcon),
+        bottomNavigationBar: CustomBottomBar(services: services, entity: entity, favIcon: favIcon),
+      ),
     );
-   }
+  }
+  
 
+  @override
+  void dispose() {
+    services.portraitModeOnly();
+    super.dispose();
+  }
+
+  Widget _buildItem(BuildContext context, entity, favIcon) {
+    return OrientationBuilder(builder: (context, orientation) {
+      return Center(
+          child: ListView(children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // recibe la imagen seleccionada y la muestra
+            InteractiveViewer(
+        boundaryMargin: EdgeInsets.all(0.0),
+        minScale: 0.1,
+        maxScale: 2.5,
+        child: AssetEntityImage(
+              entity,
+              height: orientation == Orientation.portrait ? 650 : 230,
+            ),
+      ),
+            
+          ],
+        ),
+      ]));
+    });
+  }
+}
+
+class CustomBottomBar extends StatelessWidget {
+  const CustomBottomBar({
+    Key? key,
+    required this.services,
+    required this.entity,
+    required this.favIcon,
+  }) : super(key: key);
+
+  final ImagesServices services;
+  final AssetEntity entity;
+  final IconData favIcon;
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      color: Colors.transparent,
+      elevation: 0,
+      child: IconTheme(
+    data: IconThemeData(color: Theme.of(context).colorScheme.onPrimary),
+    child: 
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                IconButton(
+                  tooltip: 'Delete',
+                    onPressed: () =>
+                        services.deleteCurrent(context, entity),
+                    icon: const Icon(Icons.delete, color: Colors.black,)),
+                IconButton(
+                  tooltip: 'Favorite',
+                    onPressed: () => services.iconChange(entity),
+                    icon: Icon(favIcon, color: Colors.black)),
+                IconButton(
+                  tooltip: 'Details',
+                    onPressed: () => services.iconChange(entity),
+                    icon: const Icon(Icons.menu, color: Colors.black))
+              ],
+            ),
+
+      ),
+    );
+  }
 }
